e4fbffa5595452aafb4a9712fc851014cd435bb3
Jello <johnwangjl@gmail.com>
2024-11-23 11:49:01 +0800

    init demo

---
+++ src/components/Canvas.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <canvas id="c1" width="800" height="800"></canvas>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted } from 'vue';
+  import * as fabric from 'fabric';
+  
+  onMounted(() => {
+    const canvas = new fabric.Canvas('c1');
+  
+    // 添加一个简单的矩形
+    canvas.add(
+      new fabric.Rect({
+        width: 200,
+        height: 200,
+        fill: 'pink',
+        top: 50,
+        left: 50,
+      })
+    );
+  });
+  </script>
+  
+  <style>
+  canvas {
+    border: 1px solid #ccc;
+    display: block;
+  }
+  </style>
